6d22554866bd5f4e2dfa9c32be9da5c26006b331
Kim Schulze <kimberlykayschulze@gmail.com>
2018-05-06 10:40:29 -0700

    Initial Setup of AngularJS site.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ClinicsComponent } from './clinics/clinics.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HistoryComponent } from './history/history.component';
+import { MissionComponent } from './mission/mission.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent,
+    AboutUsComponent,
+    ClinicsComponent,
+    ContactUsComponent,
+    HistoryComponent,
+    MissionComponent
   ],
   imports: [
     BrowserModule
